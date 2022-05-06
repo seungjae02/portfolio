ca2caa1203210f4e82bd4a42ea81365bfd01c12c
Seung Jae Yang <sjyang1126@gmail.com>
2022-05-06 02:23:38 -0400

    init commit (among us added)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Banner from './Banner';
+import Stars from './components/Stars';
+
+
+function App() {
+  return (
+    <div><Stars/></div>
+  );
+}
+
+export default App;
---
+++ src/Banner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactFloaterJs from 'react-floaterjs';
+
+
+
+class Banner extends React.Component {
+    render() { 
+        return <div>
+   
+        </div>;
+    }
+}
+ 
+export default Banner;
