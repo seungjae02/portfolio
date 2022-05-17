ffa8ee3630049564a9604006272cf38806851d69
Seung Jae Yang <sjyang1126@gmail.com>
2022-05-17 15:09:07 -0400

    social media links added

--- src/landing/Landing.js
+++ src/landing/Landing.js
@@ -15,7 +15,8 @@ function Landing() {
             <Stars/>
         
             <div className="flexbox-container center">
-                <div className="landing-message-container landing-message-style">
+                <div className="message-social-media-container">
+                    <div className="landing-message-container landing-message-style">
                         <h1>
                             Hi, my name is Jay and I am a
                             <TypeAnimation
@@ -29,12 +30,30 @@ function Landing() {
                                 repeat={Infinity}
                             />
                         </h1>
+                    </div>
+
+                    <ul className="social-media-links">
+                        <li>
+                            <a href="https://www.linkedin.com/in/sj-yang/">
+                            <i class="fab fa-linkedin-in icon"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/seungjae02">
+                            <i class="fab fa-github icon"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/jay.yang.752861">
+                            <i class="fab fa-facebook-f icon"></i></a></li>
+                        <li>
+                            <a href="mailto:seungjae.yang@mail.utoronto.ca">
+                            <i class="fa fa-envelope icon"></i></a></li>
+                    </ul>
                 </div>
 
                 
                         
                 <ReactFloaterJs className="floatingBung">
-                    <img src={SpaceBung}/>
+                    <img src={SpaceBung} className="responsive-floatingBung"/>
                 </ReactFloaterJs>
             </div>
         </div>
