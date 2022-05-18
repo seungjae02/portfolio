9253286251253e9ac6195d4ceed30a9d27fc307c
Seung Jae Yang <sjyang1126@gmail.com>
2022-05-18 15:26:16 -0400

    landing contents made responsive but needs work on the margins

--- src/landing/Landing.js
+++ src/landing/Landing.js
@@ -13,12 +13,14 @@ function Landing() {
         <div>
 
             <Stars/>
-        
+                
             <div className="flexbox-container center">
                 <div className="message-social-media-container">
                     <div className="landing-message-container landing-message-style">
                         <h1>
-                            Hi, my name is Jay and I am a
+                            Hi! <span class="wave">👋</span> 
+                            <br/>
+                            My name is SeungJae and I am a
                             <TypeAnimation
                                 cursor={true}
                                 sequence={[
@@ -31,23 +33,25 @@ function Landing() {
                             />
                         </h1>
                     </div>
-
-                    <ul className="social-media-links">
-                        <li>
-                            <a href="https://www.linkedin.com/in/sj-yang/">
-                            <i class="fab fa-linkedin-in icon"></i></a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/seungjae02">
-                            <i class="fab fa-github icon"></i></a>
-                        </li>
-                        <li>
-                            <a href="https://www.facebook.com/jay.yang.752861">
-                            <i class="fab fa-facebook-f icon"></i></a></li>
-                        <li>
-                            <a href="mailto:seungjae.yang@mail.utoronto.ca">
-                            <i class="fa fa-envelope icon"></i></a></li>
-                    </ul>
+                    
+                    <div className="social-media-links">                             
+                        <ul>
+                            <li>
+                                <a href="https://www.linkedin.com/in/sj-yang/">
+                                <i class="fab fa-linkedin-in icon"></i></a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/seungjae02">
+                                <i class="fab fa-github icon"></i></a>
+                            </li>
+                            <li>
+                                <a href="https://www.facebook.com/jay.yang.752861">
+                                <i class="fab fa-facebook-f icon"></i></a></li>
+                            <li>
+                                <a href="mailto:seungjae.yang@mail.utoronto.ca">
+                                <i class="fa fa-envelope icon"></i></a></li>
+                        </ul>
+                    </div>
                 </div>
 
                 
